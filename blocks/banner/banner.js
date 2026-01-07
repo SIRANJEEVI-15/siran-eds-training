0035a22ab9280f73eff1f903561cd2f7eef13973
Siranjeevi Margandeyan(UST <267545@ust.com>
2026-01-07 12:07:08 +0530

    banner js updating

--- blocks/banner/banner.js
+++ blocks/banner/banner.js
@@ -1,8 +1,32 @@
-export default function decorate(block) {
-  // Add your banner block functionality here
-  const items = [...block.children];
+import { createOptimizedPicture } from '../../scripts/aem.js';
+import { moveInstrumentation } from '../../scripts/scripts.js';
+import { ul, li } from '../../scripts/dom-helpers.js';
 
-  items.forEach((item) => {
-    item.classList.add('banner-item');
+export default function decorate(block) {
+  const bannerList = ul(
+    ...([...block.children].map((row) => {
+      const item = li();
+      moveInstrumentation(row, item);
+      
+      while (row.firstElementChild) item.append(row.firstElementChild);
+      
+      [...item.children].forEach((child) => {
+        if (child.children.length === 1 && child.querySelector('picture')) {
+          child.className = 'banner-item-image';
+        } else {
+          child.className = 'banner-item-body';
+        }
+      });
+      
+      return item;
+    })),
+  );
+
+  bannerList.querySelectorAll('picture > img').forEach((img) => {
+    const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
+    moveInstrumentation(img, optimizedPic.querySelector('img'));
+    img.closest('picture').replaceWith(optimizedPic);
   });
+
+  block.replaceChildren(bannerList);
 }
